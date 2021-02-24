7c02cc65afea83a6419469c5bd1b01489a415652
Jcparks44 <you@example.com>
2021-02-24 14:20:26 -0500

    Jaelynn Parks Final Project

---
+++ Hypothesis.R
@@ -0,0 +1,5 @@
+#A variety of pathogenic bacteria that develop single mutations to resist increasing concentrations of multiple antibiotics will have a lower relative doubling number in set unit time, as a result of fitness cost. 
+
+Dataset <- read.csv('~/Desktop/Evolution/Final Project/Final_Project_Data.csv', 
+	stringsAsFactors=F)
+
